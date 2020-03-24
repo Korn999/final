3b3b100c592a30bea7c4f233cd685756bedee36e
Korn999 <59908420+Korn999@users.noreply.github.com>
2020-03-24 10:02:58 +0700

    Update index.php
    แก้ 4

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <?php echo "รหัสนิสิต-62310096"; ?><br>
 <?php echo "ชื่อ-สกุล นาย สุทธิพงษ์  ปันดี."; ?><br>
 <?php echo "ชื่อเล่น กรณ์."; ?><br>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/pb7K6fy0rbo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" 
+src="https://www.youtube.com/embed/pb7K6fy0rbo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+</iframe>
 </BODY>
 </HTML>
