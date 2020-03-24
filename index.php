ac42a0aa8fe2d1f173e220e79c4b9ea322ca55a8
Korn999 <59908420+Korn999@users.noreply.github.com>
2020-03-24 10:34:53 +0700

    Update index.php
    แก้ รอบที 10

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <?php echo "รหัสนิสิต-62310096"; ?><br>
 <?php echo "ชื่อ-สกุล นาย สุทธิพงษ์  ปันดี."; ?><br>
 <?php echo "ชื่อเล่น กรณ์."; ?><br>
-<?php echo "      นี่คือวิดีโอแนะนำตัว     "; ?><br>
+<?php echo "--------นี่คือวิดีโอแนะนำตัว--------"; ?><br>
 <iframe width="560" height="315" 
 src="https://www.youtube.com/embed/pb7K6fy0rbo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
 </iframe><br>
