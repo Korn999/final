5cc556c3ea2b49dceb7cf46009338e503acf2a76
Korn999 <59908420+Korn999@users.noreply.github.com>
2020-03-24 09:58:31 +0700

    Update index.php
    video ไม่ขึ้นจ้าาา 2

--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 <H1>
 <?php echo "รหัสนิสิต-62310096"; ?><br>
 <?php echo "ชื่อ-สกุล นาย สุทธิพงษ์  ปันดี."; ?><br>
+<?php echo "ชื่อเล่น กรณ์."; ?><br>
 <iframe width="500" height="500"
-src="https://youtu.be/pb7K6fy0rbo">
+src="https://www.youtube.com/watch?v=pb7K6fy0rbo&feature=youtu.be&fbclid=IwAR2lBm0LLvOM7JLpB1IdotsDH6dfCC5WyC2EABTKSymqe29H18zj8UpWbLo">
 </iframe>
 </BODY>
 </HTML>
