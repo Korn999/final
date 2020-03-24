2f58d7e44907df03177d33f9548b7f347564d13d
Korn999 <59908420+Korn999@users.noreply.github.com>
2020-03-24 09:39:18 +0700

    Create index.php
    เย้หาเจอแล้ว 1

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-62310096"; ?><br>
+<?php echo "ชื่อ-สกุล นาย สุทธิพงษ์  ปันดี."; ?><br>
+<iframe width="500" height="500"
+src="https://youtu.be/pb7K6fy0rbo">
+</iframe>
+</BODY>
+</HTML>
